08841633a756bef71239f3178a855c8b44093f6d
hppRC <hpp.ricecake@gmail.com>
2020-05-16 01:39:09 +0900

    :+1: Update files

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const sitemap = require(`nextjs-sitemap-generator`);
 
 sitemap({
-  baseUrl: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `hpprc.github.io` : `next-tempalte.hpprc.com`,
+  baseUrl: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `hpprc.github.io` : `profile.hpprc.com`,
   pagesDirectory: `${__dirname}/src/pages`,
   targetDirectory: `public/`,
 });
 
 module.exports = {
   target: `serverless`,
-  assetPrefix: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `/next-template` : ``,
+  assetPrefix: ``,
 };
