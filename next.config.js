db290d354bf1f6fd878adff53f1a123c0358061f
hppRC <hpp.ricecake@gmail.com>
2020-05-23 02:35:30 +0900

    :sparkles: Add github pages custom deployment

--- next.config.js
+++ next.config.js
@@ -2,12 +2,10 @@
 const sitemap = require(`nextjs-sitemap-generator`);
 
 sitemap({
-  baseUrl: process.env.NEXT_PUBLIC_GITHUB_PAGES ? `hpprc.github.io` : `profile.hpprc.com`,
   pagesDirectory: `${__dirname}/src/pages`,
   targetDirectory: `public/`,
 });
 
 module.exports = {
   target: `serverless`,
-  assetPrefix: ``,
 };
