ad29e49159794e7938501b4178fc15c8f080c8cf
hppRC <hpp.ricecake@gmail.com>
2020-05-24 05:33:43 +0900

    :sparkles: Update confing

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-param-reassign */
 const sitemap = require(`nextjs-sitemap-generator`);
+const withOffline = require(`next-offline`);
 
 sitemap({
   pagesDirectory: `${__dirname}/src/pages`,
   targetDirectory: `public/`,
 });
 
-module.exports = {
+module.exports = withOffline({
   target: `serverless`,
-};
+});
