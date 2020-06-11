27034d0dbcf0ae55df9be21a88a557504597af4f
hppRC <hpp.ricecake@gmail.com>
2020-06-12 02:28:27 +0900

    :sparkles: Update dependencies and add image optimization library

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /* eslint-disable no-param-reassign */
 const sitemap = require(`nextjs-sitemap-generator`);
 const withOffline = require(`next-offline`);
+const withOptimizedImages = require(`next-optimized-images`);
 
 sitemap({
   pagesDirectory: `${__dirname}/src/pages`,
   targetDirectory: `public/`,
 });
 
-module.exports = withOffline({
-  target: `serverless`,
-});
+module.exports = withOptimizedImages(
+  withOffline({
+    target: `serverless`,
+  })
+);
