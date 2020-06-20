7aafb17230e223574e53afb6743bb40f32d74041
hppRC <hpp.ricecake@gmail.com>
2020-06-20 23:37:12 +0900

    :shower: Remove unused files

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import 'sanitize.css';
 import 'src/styles/global.scss';
-import 'src/styles/tailwind.css';
 
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
 import { Layout } from 'src/layouts';
-import { ColorModeContainer, langModeContainer } from 'src/store';
-import { ThemeProvider } from 'src/theme';
 
 // export const reportWebVitals = (metric: unknown) => {
 //   console.log(metric);
@@ -19,14 +16,8 @@ export default ({ Component, pageProps }: AppProps): JSX.Element => (
     <Head>
       <title>Hayato Tsukagoshi</title>
     </Head>
-    <langModeContainer.Provider>
-      <ColorModeContainer.Provider>
-        <ThemeProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </ThemeProvider>
-      </ColorModeContainer.Provider>
-    </langModeContainer.Provider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   </>
 );
--- src/store/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from './color-mode-container';
-export * from './lang-mode-container';
