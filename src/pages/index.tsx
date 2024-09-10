c91e86b8082242ede11c06769e3f3e73766bbeec
hppRC <hpp.ricecake@gmail.com>
2024-09-10 23:08:15 +0900

    chore

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -362,7 +362,8 @@ const Component: React.FCX = function () {
           </div>
           <div className="mx-auto flex flex-col justify-end space-y-2 p-2 md:pb-0">
             <div>
-              4th year Ph.D. student at Graduate School of Informatics, Nagoya University, Japan.
+              4th year Ph.D. student at Graduate School of Informatics,
+              NagoyaUniversity, Japan.
               {` `}
               <br className="hidden md:block" />
               <ExternalLink
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -3,9 +3,9 @@ type Theme = Readonly<{
   backgroundColor: string;
 }>;
 
-type ColorMode = 'theme-base' | 'theme-light' | 'theme-dark';
+type ColorMode = "theme-base" | "theme-light" | "theme-dark";
 
-type LangMode = 'ja-JP' | 'en-US';
+type LangMode = "ja-JP" | "en-US";
 
 type Resource = {
   title: string;
